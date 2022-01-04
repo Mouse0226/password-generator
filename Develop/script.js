48fa8776cf4c15e8c773038d31b41ab5edfa4668
Mouse0226 <mouse1987.ml@gmail.com>
2022-01-03 18:46:56 -0600

    create length

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,19 @@
+var passLength = function() {
+  var length = 0;
+  
+  while (length < 8 || length > 128) {
+    length = prompt("Please enter the desired password length between 8 and 128 characters.");
+  }
 
+  console.log("Password length is " + length);
+  return passLength;
+};
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  passLength();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,7 +22,7 @@ function writePassword() {
 }
 
 var generatePassword = function() {
-  return "password";
+  return "somerandomvalue";
 }
 
 // Add event listener to generate button
